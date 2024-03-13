f7e62cdff02af711a8d6d9c6e9431c2e176ac4af
Diego Benavides <134892736+Diego-A-B@users.noreply.github.com>
2024-03-13 10:59:16 -0400

    Nested looping.

---
+++ Repetition/Nested_loops_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int i,j, n;
+
+    n = 0;
+    for(i=6;i>n;i--){
+        for(j=0;j<i;j++){
+            printf("*");
+            
+        }
+        printf("\n");
+    }
+}
