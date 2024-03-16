14b6a7e287687dcc837dd6f787cef2bd661aed92
Diego Benavides <134892736+Diego-A-B@users.noreply.github.com>
2024-03-15 23:59:46 -0400

    working on reversing array.

---
+++ One_Dimensional_Arrays/Reverse_array _in_from_input_order.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, arr[10];
+
+
+    printf("Enter the ten numbers in your array: \n");
+
+    for(i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+    
+    }
+
+
+    printf("Your entered array:\t");
+
+    for(i=0;i<10;i++){
+        printf("%d ",arr[i]);
+    }
+
+
+    return 0;
+}
