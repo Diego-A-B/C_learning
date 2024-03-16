b578f2ad956a35024bd802e457482aafbdb7e305
Diego Benavides <134892736+Diego-A-B@users.noreply.github.com>
2024-03-15 23:45:42 -0400

    Added array methods.

--- One_Dimensional_Arrays/Intro_to_1D_Arrays.c
+++ One_Dimensional_Arrays/Intro_to_1D_Arrays.c
@@ -3,14 +3,26 @@
 int main(){
 
 int i, arr_1[10];
+int sum = 0;
+double avg;
 
 for(i=1;i<=10;i++){
-    arr_1[i] = i;
+    arr_1[i-1] = i;
 }
 
-for(i=1;i<=10;i++){
+printf("Array:\n");
+
+for(i=0;i<10;i++){
     printf("%d\n",arr_1[i]);
 }
+
+for(i=0;i<10;i++){
+    sum += arr_1[i];
+}
+
+avg = ( (double) sum ) / 10;
+printf("The sum of the array is %d\n",sum);
+printf("The mean of your array is %.2lf",avg);
 
 return 0;
 
